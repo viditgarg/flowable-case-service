adc8d60be407525a4371cfbb35230eb4d0c21042
Vidit K <viditgarg76@gmail.com>
2026-04-06 10:04:31 -0400

    flowable case service

---
+++ src/main/java/org/ny/its/flowable_case_service/FlowableCaseServiceApplication.java
@@ -0,0 +1,13 @@
+package org.ny.its.flowable_case_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowableCaseServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowableCaseServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/ny/its/flowable_case_service/FlowableCaseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ny.its.flowable_case_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowableCaseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
